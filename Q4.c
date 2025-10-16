e4629e93059409ec794ae3aa59f28df7769e5757
lucaasleal <lucasfaleal@gmail.com>
2025-10-16 10:48:18 -0300

    feat: Q4 write and read cycles

--- Q4.c
+++ Q4.c
@@ -3,17 +3,24 @@
 #include <stdlib.h>
 
 #define TAM_ARRAY 10
-#define N 2 //  Número de Threads Leitoras
-#define M 1 //  Número de Threads escritoras
+#define N 15 //  Número de Threads Leitoras
+#define M 13 //  Número de Threads escritoras
+#define WRITES_LIMIT 10
+#define READS_LIMIT 30
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t writeNow = PTHREAD_COND_INITIALIZER;
-pthread_cond_t readNow = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t gate_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t writeNow_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t readNow_cond = PTHREAD_COND_INITIALIZER;
 
 int numReadersNow = 0;
 int numWritersNow = 0;
 int numWritersWaiting = 0;
+
+int writesNow = 0;
+int readsNow = 0;
+int writersPriority = 0;
+int readersPriority = 0;
+
 int arr[TAM_ARRAY];
 
 void *read();
@@ -49,24 +56,43 @@ int main(){
 void *read(void* threadID){
     int* tid = ((int *) threadID);
     while(1){
-        pthread_mutex_lock(&gate_mutex);
         pthread_mutex_lock(&mutex);
-        while(numWritersNow>0){ //Espera enquanto alguém escreve ou está querendo escrever
-            pthread_cond_wait(&readNow, &mutex);
+        while(numWritersNow > 0 ||                                 // Escritor ativo
+              writersPriority == 1 ||                                   // É a vez dos escritores
+              (numWritersWaiting > 0 && readsNow >= READS_LIMIT)){ //Espera enquanto alguém escreve ou está querendo escrever
+            pthread_cond_wait(&readNow_cond, &mutex);
         }
+
+        if(readersPriority == 1 || readsNow>=READS_LIMIT){
+            readsNow = 0;
+            readersPriority = 0;
+        }
+        
         numReadersNow++;
+        readsNow++;
         pthread_mutex_unlock(&mutex);
-        pthread_mutex_unlock(&gate_mutex);
         
         //Leitura do dado
         int idx = rand()%TAM_ARRAY;
         int data = arr[idx];
-        printf("Value %d readed in arr[%d] by reader %d\n", data, idx, *tid);
+        printf("Value %d read in arr[%d] by reader %d\n", data, idx, *tid);
         //Liberação para outro leitor
         pthread_mutex_lock(&mutex);
         numReadersNow--;
-        if(numReadersNow==0)
-            pthread_cond_signal(&writeNow); //Acorda o primeiro escritor
+        if(numReadersNow==0){
+            if(readsNow == READS_LIMIT){
+                printf("%d reads finished\n", readsNow);
+                readsNow = 0;
+                writersPriority = 1;
+                readersPriority = 0;
+                pthread_cond_broadcast(&writeNow_cond); // Acorda todos os escritores
+            } else {
+                pthread_cond_broadcast(&readNow_cond);  // Acorda leitores
+                pthread_cond_signal(&writeNow_cond);    // Acorda um escritor
+            }
+        } else {
+            pthread_cond_signal(&writeNow_cond);    // Acorda um escritor
+        }
         pthread_mutex_unlock(&mutex);
     }
 }
@@ -74,29 +100,44 @@ void *read(void* threadID){
 void *write(void* threadID){
     int* tid = ((int *) threadID);
     while(1){
-        pthread_mutex_lock(&gate_mutex);
         pthread_mutex_lock(&mutex);
         numWritersWaiting++; //Sinaliza que quer escrever
 
-        while(numReadersNow>0 || numWritersNow>0){ //Espera enquanto alguém lê ou escreve
-            pthread_cond_wait(&writeNow, &mutex);
+        while(numReadersNow > 0 ||                                    // Leitores ativos
+              numWritersNow > 0 ||                                    // Outro escritor ativo
+              readersPriority == 1 ||                                   // É a vez dos leitores
+              (numReadersNow > 0 && writesNow >= WRITES_LIMIT)){ //Espera enquanto alguém lê ou escreve
+            pthread_cond_wait(&writeNow_cond, &mutex);
         }
+
+        if(writersPriority == 1 || writesNow>=READS_LIMIT){
+            writesNow = 0;
+            writersPriority = 0;
+        }
+
         numWritersWaiting--; //Tira a sinalização, pois conseguiu entrar como escritor
         numWritersNow = 1;  //Entra como escritor
-        pthread_mutex_unlock(&gate_mutex);
+        writesNow++;
         pthread_mutex_unlock(&mutex);
 
         //Escrita do dado
         int idx = rand()%TAM_ARRAY;
         int data = rand()%1000;
         arr[idx] = data;
-        printf("Value %d writed in arr[%d] by writer %d\n", data, idx, *tid);
+        printf("Value %d written in arr[%d] by writer %d\n", data, idx, *tid);
         
         //Liberação para outro escritor ou leitores
         pthread_mutex_lock(&mutex);
         numWritersNow = 0;
-        pthread_cond_signal(&writeNow); //Acorda o próximo escritor
-        pthread_cond_broadcast(&readNow); //Acorda os leitores
+        if(writesNow >= WRITES_LIMIT){
+            printf("%d writes finished\n", writesNow);
+            writersPriority = 0;
+            readersPriority = 1;
+            pthread_cond_broadcast(&readNow_cond); // Acorda todos os leitores
+        } else {
+            pthread_cond_broadcast(&readNow_cond);  // Acorda leitores
+            pthread_cond_broadcast(&writeNow_cond);    // Acorda um escritor
+        }
         pthread_mutex_unlock(&mutex);
     }
 }
